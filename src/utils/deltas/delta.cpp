5eec02acc8ee5db391d245a57df0f68ae5050a64
Kieran Zylstra <kieran.zylstra@colorado.edu>
2022-06-23 13:01:17 -0600

    making progress on delta class

--- src/utils/deltas/delta.cpp
+++ src/utils/deltas/delta.cpp
@@ -1,33 +1,163 @@
 #include "delta.h"
 #include "varint.h"
+#include <vector>
 
-#include <xmmintrin.h>
+#include <immintrin.h>
+
+
+int Delta::Delta(unsigned int * Bstarts, unsigned int * Bends, unsigned int Bcount)
+{
+    Delta::B_starts = Bstarts;
+    Delta::B_ends = Bends;
+    Delta::bcount = Bcount;
+
+    Delta::delta_starts = malloc(sizeof(int)*Bcount);
+    Delta::delta_ends = malloc(sizeof(int)*Bcount):
+    Delta::B_lens = malloc(sizeof(int)*Bcount);
+    
+    Delta::calculate_interval_lengths();
+    Delta::determine_bprime();
+}
 
 int Delta::compute_deltas_d2(unsigned int * Bstarts, unsigned int * Bends, unsigned int B_length)
 {
-    for (int i = 0; i < B_length; i+=2) {
-        //store first value,
-        //store second value,
-        //compute delta with simd instruction
-        u_int64_t x1s, x2s;
-        u_int64_t x1e, x2e;
-        __m64 m1s, m2s;
-        __m64 m1e, m2e;
-        x1s = Bstarts[i];
-        x1s = x1s << 32;
-        x1s = x1s | Bstarts[i+1];
-        x2s = Bstarts[i+2];
-        x2s = x2s << 32;
-        x2s = x2s | Bstarts[i+1];
+    // zeros out the registers, should be redundant
+    // ymmregister is a union defined in the header file
+    ymmregister y1 = _mm_setzero_si128();
+    ymmregister y2 = _mm_setzero_si128();
+    ymmregister yr = _mm_setzero_si128();
 
-        _mm_sub_pi32((__m64) x2s, (__m64) x1s);
+    for (int i = 0; i < B_length; i+=4) {
+
+        // sets the ymm registers and subtracts the values to be stored in the result ymm register
+        y1.r4 = _mm_setr_epi32(Bstarts[i], Bstarts[i+1], Bstarts[i+2], Bstarts[i+3]);
+        y2.r4 = _mm_setr_epi32(Bstarts[i+4], Bstarts[i+5], Bstarts[i+6], Bstarts[i+7]);
+        yr.r4 = _mm_sub_epi32(y2, y1);
+        for (int j = 0; j < 4; j++) {
+            Delta::delta_starts[i+j] = yr.i4[j];
+        }
+
+        // does the same thing  as the block above just with the ends of the B intervals
+        y1.r4 = _mm_setr_epi32(Bends[i], Bends[i+1], Bends[i+2], Bends[i+3]);
+        y2.r4 = _mm_setr_epi32(Bends[i+4], Bends[i+5], Bends[i+6], Bends[i+7]);
+        yr.r4 = _mm_sub_epi32(y2, y1);
+        for (int j = 0; j < 4; j++) {
+            Delta::delta_ends[i+j] = yr.i4[j];
+        }
+
     }
 
     return 0;
 }
 
+
+int Delta::determine_bprimes()
+{
+    // b' = min(128*b'+c(b')(b-b'+8))
+    // find largest B value
+    // in for loop counting down run through example and count examples in b_values
+    // keep one number history 
+    // as soon as number stops improving set bprim as history number
+    unsigned int largest = 0;
+    int largest_bits = 0;
+    int bprim_tmp = 0;
+    int bprim_hst = 0;
+    int bprim_opt = 0;
+    int i;
+
+    int bnsizes[32];
+
+    // determine bprim for starts
+    for (i = 0; i < Delta::bcount; i++) {
+        if(Delta::delta_starts[i] > largest) {
+            largest = Delta::delta_starts[i];
+        }
+        ++bnsizes[ceil(log2(Delta::delta_starts[i]))]
+    }
+
+    largest_bits = log2(largest);
+
+    bprim_hst = 128*i+bnsizes[largest_bits-1]*(largest_bits-(largest_bits-1)+8);
+    for (i = largest_bits-2; i > 0; i--) {
+        bprim_tmp = 128*i+bnsizes[i]*(largest_bits-i+8);
+        if (bprim_tmp < bprim_hst) {
+            bprim_opt = i;
+            break;
+        }
+        bprim_hst = bprim_tmp;
+    }
+
+    Delta::bprim_starts = bprim_opt;
+
+    // reset bnsizes
+    for (i = 0; i < 32; i++) {
+        bnsizes[i] = 0;
+    }
+
+    // determine b prime for ends
+    for (int i = 0; i < Delta::bcount; i++) {
+        if(Delta::delta_ends[i] > largest) {
+            largest = Delta::delta_ends[i];
+        }
+        ++bnsizes[ceil(log2(Delta)::delta_starts[i])]
+    }
+
+    largest_bits = log2(largest);
+
+    bprim_hst = 128 * i+bnsizes[largest_bits-1]*(largest_bits=(largest_bits-1)+9);
+    for (i = largest_bits-2; i> 0; i--) {
+        bprim_tmp = 128 *i + bnsizes[i]*(largets_bits-i+8);
+        if (bprim_tmp < bprim_hst) {
+            bprim_opt = i;
+            break;
+        }
+        bprim_hst = bprim_tmp;
+    }
+
+    Delta::bprim_ends = bprim_opt;
+
+    return 0;
+}
+
+// this is for testing another compression scheme; for later implementation
 int Delta::compress_varint()
 {
+    
+}
+
+// TODO here
+int Delta::compress_s4fastpfor()
+{
+    //TODO need to compress using two different b primes
+
+    int i;
+    int num_bits;
+    int ex_c = 0;
+    int base_arr_size;
+    int metadata_arr_size;
+    vector<int> exceptions;
+
+    // Instead of this could possibly store the bnsizes variable from the determin_bprime method
+    //  in order to count the number of sizes for each one. Then a simple addition to find how
+    //  many exceptions there are. 
+    for (i = 0; i < Delta::bcount; i++) {
+        num_bits = ceil(log2(Delta::B_starts[i]));
+        if (num_bits > Delta::bprim_starts) {
+            ex_c += 1;
+            exceptions.append(i);
+        }
+    }
+
+    // allocate base array
+    base_arr_size = ceil(((Delta::bcount - ex_c) * Delta::bprim_starts)/8);
+    Delta::comp_delta_starts = malloc(base_arr_size);
+
+
+    // allocated metadata array
+    // Format: | b | b' | 1*log2(count execptions)| ... list of index of exceptions ... |
+    // may have to chnage count of excpetions chun
+    metadata_arr_size = 2 + ceil((log2(ex_c)/8)) + ex_c;
+    Delta::meta_delta_starts = malloc(metadata_arr_size);
 
 }
 
--- src/utils/deltas/delta.h
+++ src/utils/deltas/delta.h
@@ -6,21 +6,42 @@
 using namespace std;
 
 
+union ymmregister {
+    __m128i r4;
+    unsigned int i4[4];
+};
 
 class Delta {
 
 public:
     int* varint_deltastarts;
     int* varint_ends;
-    vector<int> deltastarts;
-    vector<int> deltaends;
+    unsigned int bcount;
+    unsigned int * B_starts;
+    unsigned int * B_ends;
+
+    int * delta_starts;
+    int * delta_ends;
+    int * comp_delta_starts;
+    int * comp_delta_ends;
+    int * meta_delta_starts;
+    int * meta_delta_ends;
+    
     vector<int> initial_vals;
     
-    Delta();
+    Delta(unsigned int * Bstarts, unsigned int * Bends, unsigned int Bcount);
 
     int compute_deltas_d2(int * Bstarts, int * Bends, int Blength);
     int compress_varint();
+    int compress_s4fastpfor();
     int find_match(int * A);
+
+private:
+    unsigned int * B_lens;
+    unsigned int bprim_starts;
+    unsigned int bprim_ends;
+    
+    int determine_bprime();
 };
 
 #endif
